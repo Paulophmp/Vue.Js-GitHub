de75220422e46f6bdd936c9412aa9efcce9d242a
Paulo Henrique <paulotux.mendes@gmail.com>
2019-09-28 23:23:49 -0300

    [UPDATE] Criando novo componente

---
+++ src/components/FotosApp.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-layout column>
+    <v-flex xs12>
+      <div class="text-xs-center">
+        <v-progress-circular
+          :size="50"
+          indeterminate
+          color="primary"
+        />
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'FotosApp',
+};
+</script>
