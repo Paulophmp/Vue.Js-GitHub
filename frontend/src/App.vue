d044da46de81131eb82467731cac53df9ccd1d44
PAULINHO <paulo.mendes00@hotmail.com>
2020-03-02 17:27:35 -0300

    [UPDATE] Ajustando e migrando app utilizando o vue-cli

---
+++ frontend/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/Paulophmp/Vue.Js-GitHub"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <GitHubIssues/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import GitHubIssues from './components/GitHubIssues';
+
+export default {
+  name: 'App',
+
+  components: {
+      GitHubIssues,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
