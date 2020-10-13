70671a090ab5e0406be89e04a4a53d96029301dd
Paulo Henrique <paulotux.mendes@gmail.com>
2020-10-12 21:38:19 -0300

    [UPDATE] Criando rota para os novos componentes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import GitHubIssues from '@/components/GitHubIssues';
+import List from '@/components/ListUsersRepository.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'GitHubIssues',
+    //   path: "/",
+    //   component: GitHubIssues,
+    //   props: true,
+    //   children: [
+    //     {
+    //       path: "/user",
+    //       component: GitHubIssues
+    //     },
+    //     {
+    //       path: "/project",
+    //       component: List
+    //     }
+    //   ]
+    // },
+
+
+
+    // {
+      path: '/user',
       component: GitHubIssues,
+    },
+    {
+      path: '/project',
+      component: List,
     },
   ],
 });
